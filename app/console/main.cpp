0e5f0759616e13211edb6a442fbd42770ffcb656
Alexander Granin <graninas@gmail.com>
2018-05-20 21:55:26 +0700

    Sample cleaned.

--- app/console/main.cpp
+++ app/console/main.cpp
@@ -10,169 +10,36 @@ using namespace std;
 
 #include <stm.h>
 
-stm::STML<stm::Unit> incrementCounterArticle(const stm::TVar<int>& tCounter)
-{
-    return stm::modifyTVar<int>(tCounter, [](int i) { return i + 1; });
-}
-
 stm::STML<int> incrementCounter(const stm::TVar<int>& tCounter)
 {
-    return stm::modifyTVarRet<int>(tCounter, [](int i) { return i + 1; });
+    stm::STML<stm::Unit> modified =
+            stm::modifyTVar<int>(tCounter, [](int i) { return i + 1; });
+
+    return stm::bind<stm::Unit, int>(modified,
+                     [&](const stm::Unit&){ return stm::readTVar<int>(tCounter); });
 }
 
-stm::STML<std::optional<int>> eventualIncrement(
-        const std::function<bool(int)>& cond,
-        const stm::TVar<int>& tCounter)
-{
-    std::function<stm::STML<std::optional<int>>(int)> f = [&](int counter)
-    {
-        if (cond(counter))
-        {
-            return stm::with<int, std::optional<int>>
-                        (incrementCounter(tCounter),
-                         [](int i){ return stm::pure(std::make_optional(i)); });
-        }
-        else
-        {
-            return stm::pure<std::optional<int>>(std::nullopt);
-        }
-    };
-
-    return stm::withTVar<int, std::optional<int>>(tCounter, f);
-}
-
-stm::STML<int> guaranteedIncrement(
-        const std::function<bool(int)>& cond,
-        const stm::TVar<int>& tCounter)
-{
-    std::function<stm::STML<int>(int)> f = [&](int counter)
-    {
-        if (cond(counter))
-        {
-            return stm::with<int, int>(incrementCounter(tCounter), stm::mPure);
-        }
-        else
-        {
-            return stm::retry<int>();
-        }
-    };
-
-    return stm::withTVar<int, int>(tCounter, f);
-}
-
-stm::STML<stm::Unit> fibonacci(const stm::TVar<int>& tFib0,
-                               const stm::TVar<int>& tFib1)
-{
-    std::function<stm::STML<stm::Unit>(int, int)> f = [&](int fib0, int fib1)
-    {
-        return stm::bothVoided(stm::writeTVar(tFib0, fib1),
-                               stm::writeTVar(tFib1, fib0 + fib1));
-    };
-
-    return stm::withTVars(tFib0, tFib1, f);
-}
-
-struct FibRt
-{
-    stm::Context& context;
-    stm::TVar<int> tCounter;
-    stm::TVar<int> tFib0;
-    stm::TVar<int> tFib1;
-    int limit;
-};
-
+// Thread runtime info
 struct CounterRt
 {
-    std::mutex& logLock;
-    stm::Context& context;
-    stm::TVar<int> tCounter;
-    int limit;
-    bool odd;
-    int microsecsTimeout;
+    int thread;                // Thread number
+    std::mutex& logLock;       // Mutex for logging
+    stm::Context& context;     // STM Context
+    stm::TVar<int> tCounter;   // Shared counter TVar
 };
 
-const auto oddCond  = [](int counter) { return (counter % 2) != 0; };
-const auto evenCond = [](int counter) { return (counter % 2) == 0; };
-
-void eventualCounterWorker(const CounterRt& rt)
+// Thread worker function
+void counterWorker(const CounterRt& rt)
 {
-    int i = 0;
-    while (true)
+    for (int i = 0; i < 50; ++i)
     {
-        std::optional<int> incremented =
-                stm::atomically(rt.context,
-                                eventualIncrement(rt.odd ? oddCond : evenCond, rt.tCounter));
-        {
-            std::lock_guard g(rt.logLock);
-            std::cout << "[" << i << (rt.odd ? "] odd" : "] even") << ": "
-                      << (incremented.has_value() ? std::to_string(incremented.value())
-                                                  : "not incremented.") << std::endl;
-        }
-
-        i++;
-
-        if (i >= rt.limit)
-            return;
-
-        std::chrono::microseconds interval(rt.microsecsTimeout);
-        std::this_thread::sleep_for(interval);
+        int counter = stm::atomically(rt.context, incrementCounter(rt.tCounter));
+        std::lock_guard g(rt.logLock);
+        std::cout << "thread: [" << rt.thread << "] counter: " << counter << std::endl;
     }
 }
 
-void guaranteedCounterWorker(const CounterRt& rt)
-{
-    int i = 0;
-    while (true)
-    {
-        int incremented = stm::atomically(
-                    rt.context,
-                    guaranteedIncrement(rt.odd ? oddCond : evenCond, rt.tCounter));
-        {
-            std::lock_guard g(rt.logLock);
-            std::cout << "[" << i << (rt.odd ? "] odd" : "] even") << ": "
-                      << incremented << std::endl;
-        }
-
-        i++;
-
-        if (i >= rt.limit)
-            return;
-
-        std::chrono::microseconds interval(rt.microsecsTimeout);
-        std::this_thread::sleep_for(interval);
-    }
-}
-
-
-struct ArticleCounterRt
-{
-    int thread;
-    std::mutex& logLock;
-    stm::Context& context;
-    stm::TVar<int> tCounter;
-};
-
-void articleCounterWorker(const ArticleCounterRt& rt)
-{
-    int i = 0;
-    while (true)
-    {
-        stm::atomically(rt.context, incrementCounterArticle(rt.tCounter));
-        int counter = stm::atomically(rt.context, stm::readTVar(rt.tCounter));
-        {
-            std::lock_guard g(rt.logLock);
-            std::cout << "thread: [" << rt.thread << "] counter:" << counter << std::endl;
-        }
-
-        if (i++ >= 50)
-            return;
-
-        std::chrono::microseconds interval(1000 * 100); // 100 msecs
-        std::this_thread::sleep_for(interval);
-    }
-}
-
-void run2CompetingThreads()
+int main()
 {
     stm::Context ctx;
     stm::TVar<int> tCounter = stm::newTVarIO(ctx, 0);
@@ -180,42 +47,11 @@ void run2CompetingThreads()
     std::mutex logLock;
 
     std::vector<std::thread> threads;
-    threads.push_back(std::thread(articleCounterWorker, ArticleCounterRt {1, logLock, ctx, tCounter}));
-    threads.push_back(std::thread(articleCounterWorker, ArticleCounterRt {2, logLock, ctx, tCounter}));
+    threads.push_back(std::thread(counterWorker, CounterRt {1, logLock, ctx, tCounter}));
+    threads.push_back(std::thread(counterWorker, CounterRt {2, logLock, ctx, tCounter}));
 
     for (auto& t: threads)
         t.join();
 
-
-//    std::cout << "--> Eventual Counter: start." << endl;
-
-//    std::vector<std::thread> threads;
-//    threads.push_back(std::thread(eventualCounterWorker, CounterRt {logLock, ctx, tCounter, 25, true , 300}));
-//    threads.push_back(std::thread(eventualCounterWorker, CounterRt {logLock, ctx, tCounter, 25, false, 300}));
-
-//    for (auto& t: threads)
-//        t.join();
-
-//    int result = stm::atomically(ctx, stm::readTVar(tCounter));
-//    std::cout << "--> Eventual Counter: threads ended. Result: " << result << endl;
-
-//    std::cout << "--> Guaranteed Counter: start." << endl;
-
-//    threads.clear();
-//    stm::atomically(ctx, writeTVar(tCounter, 0));
-
-//    threads.push_back(std::thread(guaranteedCounterWorker, CounterRt {logLock, ctx, tCounter, 25, true , 400}));
-//    threads.push_back(std::thread(guaranteedCounterWorker, CounterRt {logLock, ctx, tCounter, 25, false, 400}));
-
-//    for (auto& t: threads)
-//        t.join();
-
-//    result = stm::atomically(ctx, stm::readTVar(tCounter));
-//    std::cout << "--> Guaranteed Counter: threads ended. Result: " << result << endl;
-}
-
-int main()
-{
-    run2CompetingThreads();
     return 0;
 }
